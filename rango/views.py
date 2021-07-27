b9920a49957062860f9dbfa34e35fa032ed99e9f
“zhuo <2540463w@student.gla.ac.uk>
2021-07-27 12:14:33 +0100

    Pass chapter 3 tests

--- rango/views.py
+++ rango/views.py
@@ -8,7 +8,9 @@ from django.http import HttpResponse
 
 def index(request):
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    # add this line for chapter 3 tests
+    return HttpResponse("Rango says hey there partner!<a href='/rango/about/'>About</a>")
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page.<a href='/rango/'>Index</a>")
+    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
