e6069bc468b2ca12ed2409efe123807d32615bb4
“zhuo <2540463w@student.gla.ac.uk>
2021-07-27 14:54:42 +0100

    add directory for dynamic media files and add it to settings.py add a empty template for about page

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -18,9 +18,12 @@ from django.urls import path
 from rango import views
 from django.urls import include
 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
