2a1b2d7289372486cb8db08b8039ab9893996723
“zhuo <2540463w@student.gla.ac.uk>
2021-07-28 01:02:34 +0100

    Passed all test case of chapter 5

--- rango/models.py
+++ rango/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib import admin
 
+
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
@@ -18,6 +19,7 @@ class Page(models.Model):
 
     def __str__(self):
         return self.title
+
 
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
