b5015738fca1a6e4f754b575144ca45b9c2d5dd9
“zhuo <2540463w@student.gla.ac.uk>
2021-07-30 00:24:19 +0100

    apply templates to all pages

--- rango/views.py
+++ rango/views.py
@@ -36,7 +36,7 @@ def index(request):
 
 
 def about(request):
-    context_dict = {'myname': 'zuho wang'}
+    context_dict = {'myname': 'zhuo wang'}
     return render(request, 'rango/about.html', context=context_dict)
     # return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
 
