d45b5ef192c34c83ba8a73f0db060c46d340d92a
“zhuo <2540463w@student.gla.ac.uk>
2021-07-30 01:17:24 +0100

    implement categories template

--- rango/views.py
+++ rango/views.py
@@ -37,6 +37,8 @@ def index(request):
 
 def about(request):
     context_dict = {'myname': 'zhuo wang'}
+    print(request.method)
+    print(request.user)
     return render(request, 'rango/about.html', context=context_dict)
     # return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
 
